d8cb2aee29e6cb3488f458925ca9c18a0ace0442
Ingo Schommer <ingo@silverstripe.com>
2008-11-01 15:25:39 +0000

    MINOR Collecting entities for language master table with new i18nTextCollector functionality. The table is now sorted alphabetically by namespace and entity. Entities now include more translatable statics from DataObject subclasses like $db, $has_one etc.

--- lang/en_US.php
+++ lang/en_US.php
@@ -4,46 +4,196 @@ global $lang;
 
 $lang['en_US']['ArchiveWidget']['DispBY'] = 'Display by';
 $lang['en_US']['ArchiveWidget']['MONTH'] = 'month';
+$lang['en_US']['ArchiveWidget']['PLURALNAME'] = array(
+	'Archive Widgets',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['ArchiveWidget']['SINGULARNAME'] = array(
+	'Archive Widget',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
 $lang['en_US']['ArchiveWidget']['YEAR'] = 'year';
-$lang['en_US']['BlogEntry']['CN'] = 'Content';
-$lang['en_US']['BlogEntry']['DT'] = 'Date';
+$lang['en_US']['ArchiveWidget']['db_DisplayMode'] = array(
+	'DisplayMode',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
 $lang['en_US']['BlogEntry']['AU'] = 'Author';
 $lang['en_US']['BlogEntry']['BBH'] = 'BBCode help';
+$lang['en_US']['BlogEntry']['CN'] = 'Content';
+$lang['en_US']['BlogEntry']['DT'] = 'Date';
+$lang['en_US']['BlogEntry']['PLURALNAME'] = array(
+	'Blog Entries',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['BlogEntry']['SINGULARNAME'] = array(
+	'Blog Entry',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
 $lang['en_US']['BlogEntry']['TS'] = 'Tags (comma sep.)';
-$lang['en_US']['BlogHolder']['SJ'] = 'Subject';
-$lang['en_US']['BlogHolder']['TE'] = 'For example: sport, personal, science fiction';
-$lang['en_US']['BlogHolder']['SPUC'] = 'Please separate tags using commas.';
-$lang['en_US']['BlogHolder']['POST'] = 'Post blog entry';
-$lang['en_US']['BlogHolder']['SUCTITLE'] = 'SilverStripe blog module successfully installed';
-$lang['en_US']['BlogHolder']['SUCTAGS'] = 'silverstripe, blog';
-$lang['en_US']['BlogHolder']['SUCCONTENT'] = 'Congratulations, the SilverStripe blog module has been successfully installed. This blog entry can be safely deleted. You can configure aspects of your blog (such as the widgets displayed in the sidebar) in [url=admin]the CMS[/url].';
-$lang['en_US']['BlogHolder']['RSSFEED'] = 'RSS feed of this blog';
-$lang['en_US']['BlogHolder']['HAVENTPERM'] = 'Posting blogs is an administrator task. Please log in.';
-$lang['en_US']['BlogManagementWidget']['UNM1'] = 'You have 1 unmoderated comment';
-$lang['en_US']['BlogManagementWidget']['UNMM'] = 'You have %i unmoderated comments';
-$lang['en_US']['BlogManagementWidget']['COMADM'] = 'Comment administration';
-$lang['en_US']['RSSWidget']['CT'] = 'Custom title for the feed';
-$lang['en_US']['RSSWidget']['URL'] = 'URL of RSS Feed';
-$lang['en_US']['RSSWidget']['NTS'] = 'Number of Items to show';
-$lang['en_US']['TagCloudWidget']['TILE'] = 'Title';
-$lang['en_US']['TagCloudWidget']['LIMIT'] = 'Limit number of tags';
-$lang['en_US']['TagCloudWidget']['SORTBY'] = 'Sort by';
-$lang['en_US']['TagCloudWidget']['SBAL'] = 'alphabet';
-$lang['en_US']['TagCloudWidget']['SBFREQ'] = 'frequency';
-$lang['en_US']['BlogManagementWidget.ss']['POSTNEW'] = 'Post a new blog entry';
-$lang['en_US']['BlogManagementWidget.ss']['LOGOUT'] = 'Logout';
-$lang['en_US']['BlogSummary.ss']['VIEWFULL'] = 'View full post titled -';
-$lang['en_US']['BlogSummary.ss']['POSTEDBY'] = 'Posted by';
-$lang['en_US']['BlogSummary.ss']['POSTEDON'] = 'on';
-$lang['en_US']['BlogSummary.ss']['COMMENTS'] = 'Comments';
+$lang['en_US']['BlogEntry']['db_Author'] = array(
+	'Author',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['BlogEntry']['db_Date'] = array(
+	'Date',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['BlogEntry']['db_Tags'] = array(
+	'Tags',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['BlogEntry.ss']['COMMENTS'] = 'Comments';
+$lang['en_US']['BlogEntry.ss']['EDITTHIS'] = 'Edit this post';
 $lang['en_US']['BlogEntry.ss']['POSTEDBY'] = 'Posted by';
 $lang['en_US']['BlogEntry.ss']['POSTEDON'] = 'on';
-$lang['en_US']['BlogEntry.ss']['COMMENTS'] = 'Comments';
 $lang['en_US']['BlogEntry.ss']['TAGS'] = 'Tags:';
-$lang['en_US']['BlogEntry.ss']['VIEWALLPOSTTAGGED'] = 'View all posts tagged';
-$lang['en_US']['BlogEntry.ss']['EDITTHIS'] = 'Edit this post';
 $lang['en_US']['BlogEntry.ss']['UNPUBLISHTHIS'] = 'Unpublish this post';
-$lang['en_US']['BlogHolder.ss']['VIEWINGTAGGED'] = 'Viewing entries tagged with';
+$lang['en_US']['BlogEntry.ss']['VIEWALLPOSTTAGGED'] = 'View all posts tagged';
+$lang['en_US']['BlogHolder']['HAVENTPERM'] = 'Posting blogs is an administrator task. Please log in.';
+$lang['en_US']['BlogHolder']['PLURALNAME'] = array(
+	'Blog Holders',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['BlogHolder']['POST'] = 'Post blog entry';
+$lang['en_US']['BlogHolder']['RSSFEED'] = 'RSS feed of this blog';
+$lang['en_US']['BlogHolder']['SINGULARNAME'] = array(
+	'Blog Holder',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['BlogHolder']['SJ'] = 'Subject';
+$lang['en_US']['BlogHolder']['SPUC'] = 'Please separate tags using commas.';
+$lang['en_US']['BlogHolder']['SUCCONTENT'] = 'Congratulations, the SilverStripe blog module has been successfully installed. This blog entry can be safely deleted. You can configure aspects of your blog (such as the widgets displayed in the sidebar) in [url=admin]the CMS[/url].';
+$lang['en_US']['BlogHolder']['SUCTAGS'] = 'silverstripe, blog';
+$lang['en_US']['BlogHolder']['SUCTITLE'] = 'SilverStripe blog module successfully installed';
+$lang['en_US']['BlogHolder']['TE'] = 'For example: sport, personal, science fiction';
+$lang['en_US']['BlogHolder']['db_LandingPageFreshness'] = array(
+	'LandingPageFreshness',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['BlogHolder']['db_Name'] = array(
+	'Name',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
 $lang['en_US']['BlogHolder.ss']['NOENTRIES'] = 'There are no blog entries';
+$lang['en_US']['BlogHolder.ss']['VIEWINGTAGGED'] = 'Viewing entries tagged with';
+$lang['en_US']['BlogManagementWidget']['COMADM'] = 'Comment administration';
+$lang['en_US']['BlogManagementWidget']['PLURALNAME'] = array(
+	'Blog Management Widgets',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['BlogManagementWidget']['SINGULARNAME'] = array(
+	'Blog Management Widget',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['BlogManagementWidget']['UNM1'] = 'You have 1 unmoderated comment';
+$lang['en_US']['BlogManagementWidget']['UNMM'] = 'You have %i unmoderated comments';
+$lang['en_US']['BlogManagementWidget.ss']['LOGOUT'] = 'Logout';
+$lang['en_US']['BlogManagementWidget.ss']['POSTNEW'] = 'Post a new blog entry';
+$lang['en_US']['BlogSummary.ss']['COMMENTS'] = 'Comments';
+$lang['en_US']['BlogSummary.ss']['POSTEDBY'] = 'Posted by';
+$lang['en_US']['BlogSummary.ss']['POSTEDON'] = 'on';
+$lang['en_US']['BlogSummary.ss']['VIEWFULL'] = 'View full post titled -';
+$lang['en_US']['RSSWidget']['CT'] = 'Custom title for the feed';
+$lang['en_US']['RSSWidget']['NTS'] = 'Number of Items to show';
+$lang['en_US']['RSSWidget']['PLURALNAME'] = array(
+	'R S S Widgets',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['RSSWidget']['SINGULARNAME'] = array(
+	'R S S Widget',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['RSSWidget']['URL'] = 'URL of RSS Feed';
+$lang['en_US']['RSSWidget']['db_NumberToShow'] = array(
+	'NumberToShow',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSWidget']['db_RSSTitle'] = array(
+	'RSSTitle',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSWidget']['db_RssUrl'] = array(
+	'RssUrl',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['TagCloudWidget']['LIMIT'] = 'Limit number of tags';
+$lang['en_US']['TagCloudWidget']['PLURALNAME'] = array(
+	'Tag Cloud Widgets',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['TagCloudWidget']['SBAL'] = 'alphabet';
+$lang['en_US']['TagCloudWidget']['SBFREQ'] = 'frequency';
+$lang['en_US']['TagCloudWidget']['SINGULARNAME'] = array(
+	'Tag Cloud Widget',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['TagCloudWidget']['SORTBY'] = 'Sort by';
+$lang['en_US']['TagCloudWidget']['TILE'] = 'Title';
+$lang['en_US']['TagCloudWidget']['db_Limit'] = array(
+	'Limit',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['TagCloudWidget']['db_Sortby'] = array(
+	'Sortby',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['TagCloudWidget']['db_Title'] = array(
+	'Title',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['TrackBackPing']['PLURALNAME'] = array(
+	'Track Back Pings',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['TrackBackPing']['SINGULARNAME'] = array(
+	'Track Back Ping',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['TrackBackPing']['db_BlogName'] = array(
+	'BlogName',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['TrackBackPing']['db_Excerpt'] = array(
+	'Excerpt',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['TrackBackPing']['db_Title'] = array(
+	'Title',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['TrackBackPing']['db_Url'] = array(
+	'Url',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
 
 ?>
