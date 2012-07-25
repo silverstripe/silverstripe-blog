0d90f1bac3880a03b4b5905c6b4e3817c7e2cd6d
carlos barberis <carlos@silverstripe.com>
2012-07-26 09:33:39 +1200

    MINOR: add routes yml for director rules

--- _config.php
+++ _config.php
@@ -1,9 +1,5 @@
 <?php
 
-Director::addRules(10, array(
-	'metaweblog' => 'MetaWeblogController'
-));
-
 if(class_exists('WidgetArea')) DataExtension::add_to_class('BlogTree','BlogTreeExtension');
 
 ?>
