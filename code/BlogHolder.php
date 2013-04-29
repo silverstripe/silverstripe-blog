silverstripe/silverstripe-blog

commit df544e01f27ff2840961590d19c0aff12288424d

BUGFIX: fix post in the front end when not logged in

code/BlogHolder.php
@@ -173,7 +173,7 @@ class BlogHolder_Controller extends BlogTree_Controller {
 		'date',
 		'metaweblog',
 		'postblog' => 'BLOGMANAGEMENT',
-		'post' => 'BLOGMANAGEMENT',
+		'post',
 		'BlogEntryForm' => 'BLOGMANAGEMENT',
 	);
 	
@@ -299,7 +299,7 @@ class BlogHolder_Controller extends BlogTree_Controller {
 		}
 
 		$blogentry->Status = "Published";
-		$blogentry->writeToStage("Stage");
+		$blogentry->writeToStage("Live");
 		$blogentry->publish("Stage", "Live");
 
 		$this->redirect($this->Link());

commit 5e9e8e4e8c89f8b4c63d02de7b2ccc2468699c2e

Fixed BlogEntryForm saving regression from df544e01f
Was saving to live before overwriting with draft again.
Since no write to draft ocurred before, this broke
the publish() call - e.g. visible by the test breakage
in BlogHolderFunctionalTest.

code/BlogHolder.php
@@ -310,8 +310,7 @@ class BlogHolder_Controller extends BlogTree_Controller {
 			$blogentry->Locale = $this->Locale; 
 		}
 
-		$blogentry->Status = "Published";
-		$blogentry->writeToStage("Live");
+		$blogentry->writeToStage("Stage");
 		$blogentry->publish("Stage", "Live");
 
 		$this->redirect($this->Link());