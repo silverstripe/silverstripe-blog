silverstripe/silverstripe-blog

commit fe5a978f3a84a754a06f5ae06a0f9b6c11cb9a73

Finnish translation, and a few more localized strings
Thanks to @mediaclinic, see https://github.com/silverstripe/silverstripe-blog/pull/95

templates/Includes/BlogSummary.ss
@@ -3,7 +3,7 @@
 	<p class="authorDate"><% _t('BlogSummary_ss.POSTEDBY', 'Posted by') %> $Author.XML <% _t('BlogSummary_ss.POSTEDON', 'on') %> $Date.Long | <a href="$Link#PageComments_holder" title="View Comments Posted">$Comments.Count <% _t('BlogEntry_ss.COMMENTS', 'Comments') %></a></p>
 	<% if TagsCollection %>
 		<p class="tags">
-			Tags:
+			<% _t('BlogSummary_ss.TAGS','Tags' %>:
 			<% loop TagsCollection %>
 				<a href="$Link" title="View all posts tagged '$Tag'" rel="tag">$Tag</a><% if not Last %>,<% end_if %>
 			<% end_loop %>
@@ -16,5 +16,13 @@
 		<p>$Content.FirstParagraph(html)</p>
 	<% end_if %>
 	
-	<p class="blogVitals"><a href="$Link#PageComments_holder" class="comments" title="View Comments for this post">$Comments.Count comments</a> | <a href="$Link" class="readmore" title="Read Full Post">Read the full post</a></p>
+	<p class="blogVitals">
+		<a href="$Link#PageComments_holder" class="comments" title="View Comments for this post">
+			$Comments.Count <% _t('BlogSummary_ss.SUMMARYCOMMENTS','comment(s)' %>
+		</a> 
+		| 
+		<a href="$Link" class="readmore" title="Read Full Post">
+			<% _t('BlogSummary_ss.READFULLPOST','Read the full post' %>
+		</a>
+	</p>
 </div>

lang/en.yml
@@ -57,6 +57,9 @@ en:
     POSTEDBY: "Posted by"
     POSTEDON: "on"
     VIEWFULL: "View full post titled -"
+    READFULLPOST: 'Read full post'
+    SUMMARYCOMMENTS: 'comment(s)'
+    TAGS: 'Tags:'
   BlogTree:
     # 50
     PLURALNAME: "Blog Tres"

lang/fi.yml
@@ -1,0 +1,98 @@
+fi:
+  ArchiveWidget:
+    DispBY: "Näytä »"
+    MONTH: "kuukausi"
+    # 50
+    PLURALNAME: "Arkistovimpaimet"
+    # 50
+    SINGULARNAME: "Arkistovimpain"
+    YEAR: "vuosi"
+  BlogEntry:
+    AU: "Kirjoittaja"
+    BBH: "BBCode apu"
+    CN: "Sisältö"
+    DT: "Pvm"
+    # 50
+    PLURALNAME: "Blogikirjoitukset"
+    # 50
+    SINGULARNAME: "Blogikirjoitus"
+    TS: "Avainsanat (erottimena pilkku)"
+  BlogEntry_ss:
+    COMMENTS: "Kommentit"
+    EDITTHIS: "Muokkaa tätä kirjoitusta"
+    POSTEDBY: "Lähettäjä"
+    POSTEDON: "»"
+    TAGS: "Avainsanat:"
+    UNPUBLISHTHIS: "Poista julkaisusta"
+    VIEWALLPOSTTAGGED: "Näytä kaikki tägätyt kirjoitukset"
+  BlogHolder:
+    # 50
+    PLURALNAME: "Blogin koontisivut"
+    POST: "Kirjoita blogi"
+    RSSFEED: "RSS-syöte näistä blogeista"
+    # 50
+    SINGULARNAME: "Blogin koontisivu"
+    SJ: "Aihe"
+    SPUC: "Erota avainsanat pilkulla."
+    SUCCONTENT: "Onnittelut, SilverStripe blogimoduuli on asennettu onnistuneesti. Voit poistaa tämän blogikirjoituksen turvallisesti. Voit säätää blogin toiminnaliisuuksia (kuten sivupalkin toiminnallisuuksia) ylläpidosta [url=admin]the CMS[/url]."
+    SUCTAGS: "silverstripe, blog"
+    SUCTITLE: "SilverStripe blogimoduuli asennettu onnistuneesti"
+    TE: "Esim. urheilu, henkilökohtainen, sci-fi"
+  BlogHolder_ss:
+    NOENTRIES: "Ei blogikirjoituksia"
+    VIEWINGTAGGED: "Näytä kirjoitukset tällä avainsanalla"
+  BlogManagementWidget:
+    COMADM: "Kommenttien moderointi"
+    # 50
+    PLURALNAME: "Blogin hallinnointivimpaimet"
+    # 50
+    SINGULARNAME: "Blogin hallinnointivimpain"
+    UNM1: "Sinulla on yksi moderoimaton kommentti"
+    UNMM: "Sinulla on %i moderoimatonta kommenttia"
+  BlogManagementWidget_ss:
+    LOGOUT: "Kirjaudu ulos"
+    POSTNEW: "Kirjoita uusi blogiteksti"
+  BlogSummary_ss:
+    COMMENTS: "Kommentit"
+    POSTEDBY: "Lähettänyt »"
+    POSTEDON: "pvm"
+    READFULLPOST: 'Lue koko kirjoitus'
+    SUMMARYCOMMENTS: 'kommentti(a)'
+    TAGS: 'Avainsanat:'
+    VIEWFULL: "Näytä koko kirjoitus otsikolla -"
+  BlogTree:
+    # 50
+    PLURALNAME: "Blogi hakemistopuut"
+    # 50
+    SINGULARNAME: "Blogihakemistopuu"
+  RSSWidget:
+    CT: "Oma otsikko syötteelle"
+    NTS: "Montako kohdetta näytetään"
+    # 50
+    PLURALNAME: "R S S vimpain"
+    # 50
+    SINGULARNAME: "R S S vimpain"
+    URL: "Toisen sivuston URL-osoite RSS syötteeseen.  Varmistathan, että osoite on RSS-syötteeseen."
+  SubscribeRSSWidget:
+    # 50
+    PLURALNAME: "Tilaa R S S vimpaimet"
+    # 50
+    SINGULARNAME: "Tilaa R S S vimpain"
+  SubscribeRSSWidget_ss:
+    SUBSCRIBETEXT: "Tilaa"
+    SUBSCRIBETITLE: "Tilaa tämän blogin RSS"
+  TagCloudWidget:
+    LIMIT: "Rajoita avainsanojen määrää"
+    # 50
+    PLURALNAME: "Avainsanapilvi vimpaimet"
+    SBAL: "aakkoset"
+    SBFREQ: "toistuvuus"
+    # 50
+    SINGULARNAME: "Avainsanapilvi vimpain"
+    SORTBY: "Järjestä"
+    TILE: "Otsikko"
+  TrackBackPing:
+    # 50
+    PLURALNAME: "Track Back Pings"
+    # 50
+    SINGULARNAME: "Track Back Ping"

commit c553a536ba5c5e76d21f3b4939554fc1f855a074

BUG Fixing template errors (brackets not closed) introduced in fe5a978

templates/Includes/BlogSummary.ss
@@ -3,7 +3,7 @@
 	<p class="authorDate"><% _t('BlogSummary_ss.POSTEDBY', 'Posted by') %> $Author.XML <% _t('BlogSummary_ss.POSTEDON', 'on') %> $Date.Long | <a href="$Link#PageComments_holder" title="View Comments Posted">$Comments.Count <% _t('BlogEntry_ss.COMMENTS', 'Comments') %></a></p>
 	<% if TagsCollection %>
 		<p class="tags">
-			<% _t('BlogSummary_ss.TAGS','Tags' %>:
+			<% _t('BlogSummary_ss.TAGS','Tags') %>:
 			<% loop TagsCollection %>
 				<a href="$Link" title="View all posts tagged '$Tag'" rel="tag">$Tag</a><% if not Last %>,<% end_if %>
 			<% end_loop %>
@@ -18,11 +18,11 @@
 	
 	<p class="blogVitals">
 		<a href="$Link#PageComments_holder" class="comments" title="View Comments for this post">
-			$Comments.Count <% _t('BlogSummary_ss.SUMMARYCOMMENTS','comment(s)' %>
+			$Comments.Count <% _t('BlogSummary_ss.SUMMARYCOMMENTS','comment(s)') %>
 		</a> 
 		| 
 		<a href="$Link" class="readmore" title="Read Full Post">
-			<% _t('BlogSummary_ss.READFULLPOST','Read the full post' %>
+			<% _t('BlogSummary_ss.READFULLPOST','Read the full post') %>
 		</a>
 	</p>
 </div>