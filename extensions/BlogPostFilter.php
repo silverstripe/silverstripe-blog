248c5189e3fea16ac59ef239302e5123f0716e06
micmania1 <micmania@hotmail.co.uk>
2013-08-23 16:22:13 +0100

    Add lazy load fix

--- extensions/BlogPostFilter.php
+++ extensions/BlogPostFilter.php
@@ -29,4 +29,16 @@ class BlogPostFilter extends DataExtension {
 
 	}
 
+	/**
+	 * This is a fix so that when we try to fetch subclasses of BlogPost,
+	 * lazy loading includes the BlogPost table in its query. Leaving this table out
+	 * means the default sort order column PublishDate causes an error.
+	**/
+	public function augmentLoadLazyFields(SQLQuery &$query, &$dataQuery, $parent) {
+
+		// Ensures that we're joining the BlogPost table which holds required db fields.
+		$dataQuery->innerJoin("BlogPost", "`SiteTree`.`ID` = `BlogPost`.`ID`");
+
+	}
+
 }
