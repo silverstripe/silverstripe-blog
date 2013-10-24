adbefd648e3f0e9d78d7b12e0deda5b8ebca6434
Björn Radon <bjorn.radon@gmail.com>
2013-10-24 21:21:30 +0200

    removed ref to InheritSideBar

--- code/BlogTree.php
+++ code/BlogTree.php
@@ -24,12 +24,10 @@ class BlogTree extends Page {
 	
 	private static $db = array(
 		'Name' => 'Varchar(255)',
-		'InheritSideBar' => 'Boolean',
 		'LandingPageFreshness' => 'Varchar',
 	);
 	
 	private static $defaults = array(
-		'InheritSideBar' => True
 	);
 	
 	private static $has_one = array();
