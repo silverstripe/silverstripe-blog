9a77be622684944e9f5a177ef302f7809f393ac7
micmania1 <micmania@hotmail.co.uk>
2014-07-27 06:34:43 +0100

    FIX Fixed the beforeUpdateCMSFields and made them consistent

--- model/BlogPost.php
+++ model/BlogPost.php
@@ -59,13 +59,8 @@ class BlogPost extends Page {
 
 	public function getCMSFields() {
 
-	  	// Assign to variable & pass for PHP <= 5.4 closure compatibility
-	  	$data = array(
-	  		"TagsMap" => $this->Parent()->Tags()->map()->toArray(),
-	  		"CategoryMap" => $this->Parent()->Categories()->map()->toArray()
-  		);
-
-	  	$this->beforeUpdateCMSFields(function($fields) use ($data) {
+		$self =& $this;
+		$this->beforeUpdateCMSFields(function($fields) use ($self) {
 			// Add Publish date fields
 			$fields->insertAfter(
 				$publishDate = DatetimeField::create("PublishDate", _t("BlogPost.PublishDate", "Publish Date")), 
@@ -74,12 +69,18 @@ class BlogPost extends Page {
 			$publishDate->getDateField()->setConfig("showcalendar", true);
 
 			// Add Categories & Tags fields
-	     	$categoriesField = ListboxField::create("Categories", 
-	     		_t("BlogPost.Categories", "Categories"), $data['CategoryMap'])->setMultiple(true);
+			$categoriesField = ListboxField::create(
+				"Categories", 
+				_t("BlogPost.Categories", "Categories"), 
+				$self->Parent()->Categories()->map()->toArray()
+			)->setMultiple(true);
 			$fields->insertAfter($categoriesField, "PublishDate");
 
-	      	$tagsField = ListboxField::create("Tags", _t("BlogPost.Tags", "Tags"), $data['TagsMap'])
-				->setMultiple(true);
+			$tagsField = ListboxField::create(
+				"Tags", 
+				_t("BlogPost.Tags", "Tags"), 
+				$self->Parent()->Tags()->map()->toArray()
+			)->setMultiple(true);
 			$fields->insertAfter($tagsField, "Categories");
 
 			// Add featured image
@@ -87,10 +88,10 @@ class BlogPost extends Page {
 				$uploadField = UploadField::create("FeaturedImage", _t("BlogPost.FeaturedImage", "Featured Image")),
 				"Content"
 			);
-        	$uploadField->getValidator()->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'));
-	    });
+			$uploadField->getValidator()->setAllowedExtensions(array('jpg', 'jpeg', 'png', 'gif'));
+		});
 
-	  	$fields = parent::getCMSFields();
+		$fields = parent::getCMSFields();
 		return $fields;
 	}
 
