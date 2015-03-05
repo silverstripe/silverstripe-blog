89eac300c9b1843c8cbc1da172b954e4e0c6f838
Damian Mooyman <damian@silverstripe.com>
2015-03-06 08:57:43 +1300

    API Enable rename of widgets

--- code/widgets/BlogTagsWidget.php
+++ code/widgets/BlogTagsWidget.php
@@ -3,22 +3,36 @@
 if(class_exists("Widget")) {
 
 	class BlogTagsWidget extends Widget {
-		
+
 		private static $title = "Tags";
 
 		private static $cmsTitle = "Blog Tags";
 
 		private static $description = "Displays a list of blog tags.";
 
-		private static $db = array();
+		private static $db = array(
+			"Title" => "Varchar(255)",
+		);
 
 		private static $has_one = array(
 			"Blog" => "Blog",
 		);
 
+		public function Title() {
+			return $this->getField('Title') ?: parent::Title();
+		}
+
+		public function populateDefaults() {
+			parent::populateDefaults();
+			$this->setField('Title', parent::Title());
+		}
+
 		public function getCMSFields() {
 			$fields = FieldList::create();
-			$fields->push(DropdownField::create("BlogID", _t("BlogTagsWidget.Blog", "Blog"), Blog::get()->map()));
+			$fields->merge(array(
+				TextField::create('Title', 'Title', null, 255),
+				DropdownField::create("BlogID", _t("BlogTagsWidget.Blog", "Blog"), Blog::get()->map())
+			));
 			$this->extend("updateCMSFields", $fields);
 			return $fields;
 		}
@@ -34,7 +48,6 @@ if(class_exists("Widget")) {
 	}
 
 	class BlogTagsWidget_Controller extends Widget_Controller {
-		
-	}
 
+	}
 }
