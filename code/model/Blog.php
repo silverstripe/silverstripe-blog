efcdd64cf48142ae6b6ca1612f18185bfe610926
Damian Mooyman <damian@silverstripe.com>
2015-04-23 16:06:56 +1200

    Add helper class for cms styling

--- code/model/Blog.php
+++ code/model/Blog.php
@@ -108,6 +108,7 @@ class Blog extends Page implements PermissionProvider {
 				$categories,
 				$tags
 			));
+			$fields->findOrMakeTab("Root.Categorisation")->addExtraClass('blog-cms-categorisation');
 		});
 
 		$fields = parent::getCMSFields();
