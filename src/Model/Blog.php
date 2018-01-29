1feaa52ed762812637c862b60c7a15ee3eb87ff4
Raissa North <raissa@silverstripe.com>
2018-01-29 16:16:45 +1300

    ENHANCEMENT Update hard reference to png file

--- src/Model/Blog.php
+++ src/Model/Blog.php
@@ -126,7 +126,7 @@ class Blog extends Page implements PermissionProvider
      */
     private static $description = 'Adds a blog to your website.';
 
-    private static $icon = 'blog/images/site-tree-icon.png';
+    private static $icon = 'silverstripe/blog:client/images/site-tree-icon.png';
 
     /**
      * {@inheritdoc}
