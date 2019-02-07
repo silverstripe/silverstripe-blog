d1ea6bb839de1f7c4c120f7b857374120ef46091
Robbie Averill <robbie@silverstripe.com>
2019-02-07 12:05:32 +0300

    FIX Categories and tags now respect multibyte url configuration
    Previously multibyte URLs would always work, regardless of URLSegmentFilter::$default_allow_multibyte.
    This change means that they will only work when the setting is enabled.

--- src/Model/Blog.php
+++ src/Model/Blog.php
@@ -17,6 +17,8 @@ use SilverStripe\Forms\NumericField;
 use SilverStripe\ORM\DataList;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\DB;
+use SilverStripe\ORM\HasManyList;
+use SilverStripe\ORM\ManyManyList;
 use SilverStripe\ORM\SS_List;
 use SilverStripe\ORM\UnsavedRelationList;
 use SilverStripe\Security\Group;
