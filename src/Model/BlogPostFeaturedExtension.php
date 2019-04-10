fdeb1018829678868643bf938792f779eaf9a13b
Peter Thaleikis <peter.thaleikis@gmail.com>
2019-04-10 20:31:25 +0700

    MINOR: implementing featured blog posts as a widget

---
+++ src/Model/BlogPostFeaturedExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SilverStripe\Blog\Model;
+
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\CheckboxField;
+
+/**
+ * Adds a checkbox field for featured blog posts widget.
+ */
+class BlogPostFeaturedExtension extends DataExtension
+{
+    /**
+     * @var array
+     */
+    private static $db = [
+        'FeaturedInWidget' => 'Boolean',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        // Add the checkbox in.
+        $fields->addFieldToTab(
+            'Root.PostOptions',
+            new CheckboxField("FeaturedInWidget", _t(__CLASS__ . '.FEATURED', 'Include Post in Feature Widget'))
+        );
+
+        return $fields;
+    }
+}
---
+++ src/Widgets/BlogFeaturedPostsWidgetController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SilverStripe\Blog\Widgets;
+
+use SilverStripe\Widgets\Model\WidgetController;
+
+if (!class_exists(WidgetController::class)) {
+    return;
+}
+
+class BlogFeaturedPostsWidgetController extends WidgetController
+{
+
+}
